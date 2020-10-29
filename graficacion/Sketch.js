8be1418d1331411efa4f0f903d555e026f48b446
Antonio Medina <61975779+Antonio832@users.noreply.github.com>
2020-10-29 16:04:53 -0700

    finished doc pend

--- graficacion/Sketch.js
+++ graficacion/Sketch.js
@@ -13,10 +13,6 @@ var l = function (p) {
     x2in = p.select('#x2input');
     y2in = p.select('#y2input');
 
-    x1 = 0;
-    y1 = 0;
-    x2 = 0;
-    y2 = 0;
   };
 
   p.draw = function () {
@@ -246,7 +242,7 @@ var e = function (p) { //p es el handler para p5
     d1 = p.pow(b, 2) - (p.pow(a, 2) * b) + (p.pow(a, 2) / 4);
 
     P4(x, y);
-  
+
     while ((p.pow(a, 2) * (y - (0.5))) > (p.pow(b, 2) * (x + 1))) {
       if (d1 < 0) {
         d1 += (p.pow(b, 2) * (2 * x + 3));
@@ -290,14 +286,19 @@ var sketchCirculo = new p5(e, 'ellipse');
 
 //Sketch de la Parabola******************************************************************************
 var pa = function (p) { //p es el handler para p5
+  let pin;
+  let pa;
 
   p.setup = function () {
     p.createCanvas(_width, _height);
+    pin = p.createSlider(1, 100, 5);
+    pin.parent("pIn");
+    pin.style('width', '100px');
   };
 
   p.draw = function () {
     p.background(0);
-    
+
     p.push();
     p.stroke(255);
     p.strokeWeight(2);
@@ -305,16 +306,69 @@ var pa = function (p) { //p es el handler para p5
     p.line(_width / 2, 0, _width / 2, _height);
     p.pop();
 
-    p.noLoop();
+    pa = pin.value();
+    p.translate(_width / 2, _height / 2);
+    p.scale(1, -1);
+
+    _parabola(pa);
   };
+
+  function _parabola(param) {
+    let x = 0;
+    let y = 0;
+    let d1, d2;
+    d1 = p.pow(x + 1.0, 2) - 4 * (param * y + 0.5);
+
+    //REGION I
+    while (y < param && x <= 400) {
+      x++;
+      if (d1 <= 0) {
+        //E
+        d1 += 2 * x - 3;
+      } else {
+        //NE
+        d1 += 2 * x + 3 - 4 * param;
+        y++;
+      }
+      P2(x, y);
+    }
+
+    //REGION II
+    d2 = p.pow(x + 0.5, 2) - 4 * (param * (y + 1.0));
+    while (x <= 400) {
+      y++;
+      if (d2 <= 0) {
+        //NE
+        d2 += 2 * x - 4 * param + 2;
+        x++;
+      } else {
+        //N
+        d2 += -4 * param;
+      }
+
+      P2(x, y);
+    }
+  }
+
+  function P2(x, y) {
+
+    p.stroke(0, 255, 0);
+    p.point(x, y);
+    p.point(-x, y)
+  }
 };
 var sketchCirculo = new p5(pa, 'parabola');
 
 //Sketch de la Hiperbola*****************************************************************************
 var h = function (p) { //p es el handler para p5
+  let ain, bin;
+  let a, b;
 
   p.setup = function () {
     p.createCanvas(_width, _height);
+
+    ain = p.select('#aInHip');
+    bin = p.select('#bInHip');
   };
 
   p.draw = function () {
@@ -326,7 +380,63 @@ var h = function (p) { //p es el handler para p5
     p.line(_width / 2, 0, _width / 2, _height);
     p.pop();
 
-    p.noLoop();
+    a = parseInt(ain.value());
+    b = parseInt(bin.value());
+
+    p.translate(_width / 2, _height / 2);
+    _hiperbola(a, b);
+
   };
+
+  function _hiperbola(a, b) {
+    x = a;
+    y = 0;
+
+    let aa = a * a;
+    let bb = b * b;
+    let d1, d2;
+    //double d1 = 2*aa - bb - 2*a*bb;
+
+    d1 = (bb * p.pow(x + 0.5, 2)) - (aa * p.pow(y + 1.0, 2)) - (aa * bb);
+
+    //REGION I
+    while (x <= 500) {
+      y++;
+      if (d1 > 0) {
+        //N
+        d1 += -2 * aa * y - 3 * aa;
+      } else {
+        //NE
+        x++;
+        d1 += 2 * bb * x + 2 * bb - 2 * aa * y - 3 * aa;
+      }
+      P4(x, y);
+    }
+
+    //REGION II
+    if (b > a) {
+      d2 = (bb * p.pow(x + 1.0, 2)) - (aa * p.pow(y + 0.5, 2)) - (aa * bb);
+      while (x <= 500) {
+        x++;
+        if (d2 > 0) {
+          //NE
+          y++;
+          d2 += (2 * bb * x) + (3 * bb) - (3 * aa * y) - (2 * aa);
+        } else {
+          //E
+          d2 += -2 * bb * x - 3 * bb;
+        }
+        P4(x, y);
+      }
+    }
+  }
+
+  function P4(x, y) {
+    p.stroke(0, 255, 0);
+    p.point(x, y);
+    p.point(x, -y);
+    p.point(-x, -y);
+    p.point(-x, y);
+  }
 };
 var sketchCirculo = new p5(h, 'hiperbola');
